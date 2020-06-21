1c062e24258495974dfc51f5570d8fdc3636072e
Hamza Arfaoui <harfaoui@netisse.com>
2020-06-21 16:26:02 +0200

    ssss

--- src/Document/mySite.php
+++ src/Document/mySite.php
@@ -7,9 +7,9 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @MongoDB\Document(repositoryClass="App\Repository\MySiteRepository")
+ * @MongoDB\Document
  */
-class MySite
+class Valeurs
 {
     /**
      * @MongoDB\Id
@@ -19,7 +19,14 @@ class MySite
     /**
      * @MongoDB\Field(type="string")
      */
-    protected $nbrVisite;
+    protected $name;
+    
+    /** @MongoDB\ReferenceMany(targetDocument="Products", inversedBy="valeurs") */
+    protected $products;
+    
+    /** 
+     * @MongoDB\ReferenceOne(targetDocument="Caracteristiques", inversedBy="valeurs") */
+    protected $caracteristique = array();
     
     /**      * @return mixed      */
     public function getId()
@@ -28,13 +35,83 @@ class MySite
     }
     
     /**      * @return mixed      */
-    public function getNbrVisite()
+    public function getName()
     {
-        return $this->nbrVisite;
+        return $this->name;
     }
-    /**      * @param mixed $nbrVisite      */
-    public function setNbrVisite($nbrVisite)
+    /**      * @param mixed $name      */
+    public function setName($name)
     {
-        $this->nbrVisite = $nbrVisite;
+        $this->name = $name;
+    }
+    
+    /**
+     * Add articleRevision
+     *
+     * @param App\Document\Products $product
+     */
+    public function addProduct(Products $product)
+    {
+        $this->products[] = $product;
+    }
+    
+    /**
+     * Remove product
+     *
+     * @param App\Document\Article $product
+     */
+    public function removeProduct(Products $product)
+    {
+        $this->products->removeElement($product);
+    }
+    
+    /**
+     * Get valeurs
+     *
+     * @return \Doctrine\Common\Collections\Collection $products
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+    /**
+     * @param Products $product
+     *
+     * @return self
+     */
+    public function setProduct(Products $product)
+    {
+        $this->product = $product;
+        return $this;
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getCaracteristique()
+    {
+        return $this->caracteristique;
+    }
+    /**
+     * @param Caracteristiques $caracteristique
+     *
+     * @return self
+     */
+    public function setCaracteristique(Caracteristiques $caracteristique)
+    {
+        $this->caracteristique = $caracteristique;
+        return $this;
+    }
+    
+    public function __toString() {
+        return $this->getName();
     }
 }
